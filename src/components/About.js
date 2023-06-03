19cac78fade50237fbc409c362cac62f766ebe69
Ahha0801 <128229173+Ahha0801@users.noreply.github.com>
2023-06-03 15:53:24 -0500

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ function About() {
           <h1>About Me</h1>
           <p>
             Welcome to my portfolio! I am a passionate web developer with experience in React, JavaScript, HTML, and CSS.
+            I enjoy creating innovative and user-friendly websites that provide a seamless browsing experience.
           </p>
         </section>
     )
